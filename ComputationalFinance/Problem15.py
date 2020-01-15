7982564bc3dca5bd9bc40fcfd0a9653d52a17b9a
jetpotion <william.zhang@stonybrook.edu>
2020-01-15 16:38:48 -0500

    Create Problem15.py

---
+++ ComputationalFinance/Problem15.py
@@ -0,0 +1,44 @@
+
+
+#Finds the unique triplets of 3 numbers
+#Runs in n^2 + nlogn + n 
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) < 3:
+             return []
+        if len(nums) == 3 and sum(nums) == 0:
+            return [nums]
+        if all(v == 0 for v in nums):
+            return [[0,0,0]]
+        dictionary = {}
+        for x in range(len(nums)):
+            dictionary[nums[x]] = x
+        currentlist = []
+        for j in range(len(nums)):
+            for h in  range(len(nums)):
+                if j != h:
+                    if nums[j] + nums[h] != 0:
+                        first =   [nums[j],nums[h]]
+                        totalsum =sum(first)
+                        complement  = 0 - totalsum
+                        if complement in dictionary and dictionary[complement] != j and dictionary[complement] != h:
+                            newlist = [nums[j],nums[h],complement]
+                            if(newlist not in currentlist):
+                                currentlist.append(newlist)
+                    elif  nums[j] == 0 and nums[h] == 0:
+                        first =   [nums[j],nums[h]]
+                        totalsum =sum(first)
+                        complement  = 0 - totalsum
+                        if complement in dictionary and dictionary[complement] != j and dictionary[complement] != h:
+                            newlist = [nums[j],nums[h],complement]
+                            if(newlist not in currentlist):
+                                currentlist.append(newlist)
+        for x in currentlist:
+            x = x.sort()
+    
+        newlist = []
+        for x in currentlist:
+            if x not in newlist:
+                newlist.append(x)
+        return newlist
